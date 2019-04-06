f0329de2e7295533a2b6aaa93349a9855f5bbad8
Aji Priyo Wibowo <ajipwch@gmail.com>
2019-04-06 21:06:55 +0700

    Edit file hello.py and add feature show date

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,11 @@
+import datetime
+now = datetime.datetime.now()
+
 print("hello bujank")
+print("Hello User! \n")
+
+print("Get Gud to GIT ! \n\n")
+
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
